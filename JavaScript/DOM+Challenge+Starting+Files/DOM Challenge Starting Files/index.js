28eccc9a11313b8d6102a806a9988119f55cbc40
smaraba <smaraba@ncsu.edu>
2024-03-14 14:55:10 -0500

    JS upto DOM

---
+++ JavaScript/DOM+Challenge+Starting+Files/DOM Challenge Starting Files/index.js
@@ -0,0 +1,6 @@
+
+document.querySelector("ul").lastElementChild.innerHTML = "dog";
+document.querySelector("li a").innerHTML = "cat";
+document.querySelector("button").style.backgroundColor= "yellow";
+
+document.querySelector("h1").classList.toggle("huge");
---
+++ JavaScript/Dicee+Challenge+-+Starting+Files/index.js
@@ -0,0 +1,20 @@
+
+alert("Begin by refreshing.");
+
+let p1 = Math.min(6,Math.floor(Math.random()*6)+1);
+let p2 = Math.min(6,Math.floor(Math.random()*6)+1);
+
+let result = "Draw!"
+if (p1 > p2){
+    result = "🚩Player 1 Wins!";
+}
+else if (p1 < p2){
+    result = "Player 2 Wins!🚩";
+}
+document.getElementsByClassName("img1")[0].setAttribute("src", './images/dice'+String(p1)+'.png');
+document.querySelector(".img2").setAttribute("src", './images/dice'+String(p2)+'.png');
+
+document.querySelector("h1").textContent = result;
+
+
+
