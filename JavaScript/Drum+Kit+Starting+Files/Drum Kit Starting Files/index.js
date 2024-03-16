fae80e734197bb09988cbbccb2d5d1601944e7d8
smaraba <smaraba@ncsu.edu>
2024-03-16 13:05:34 -0500

    DOM advanced

---
+++ JavaScript/Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
@@ -0,0 +1,28 @@
+const drums = document.getElementsByClassName("drum");
+const maps = {'w': "tom-1", 'a': "tom-2", 's': "tom-3", 'd': "tom-4", 'j': "snare", 'k': "kick-bass", 'l': "crash"};
+
+for (let i = 0; i < drums.length; i++) {
+
+// Using an IIFE to create a closure that captures 'i' and 'maps' for each loop iteration
+    drums[i].addEventListener("click", function() {
+        console.log(this.innerHTML);
+        makeSound(this.innerHTML);
+    });
+}
+
+document.addEventListener("keydown", function(event) {
+    console.log(event.key);
+    if(event.key in maps){
+    makeSound(event.key);
+}
+});
+
+function makeSound(i) {
+    var audio = new Audio("./sounds/" + maps[i] + ".mp3");
+    document.querySelector("."+i).classList.add("pressed");
+    setTimeout(function(){
+        document.querySelector("."+i).classList.remove("pressed");
+            },100);
+    audio.play();
+
+}
